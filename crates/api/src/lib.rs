156099b1ce49de2c7e0a6d5b015a67f9c9379cc7
uuhan <xuminhui189@gmail.com>
2022-01-14 18:27:33 +0800

    node_api_rs::Napi*

--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -7,6 +7,50 @@ pub mod api {
     include!(concat!(env!("OUT_DIR"), "/api.rs"));
 }
 
+/// napi_property_attributes
+#[repr(u32)]
+pub enum NapiPropertyAttributes {
+    Default = api::napi_property_attributes_napi_default,
+    Writable = api::napi_property_attributes_napi_writable,
+    Enumerable = api::napi_property_attributes_napi_enumerable,
+    Configurable = api::napi_property_attributes_napi_configurable,
+    Static = api::napi_property_attributes_napi_static,
+    DefaultMethod = api::napi_property_attributes_napi_default_method,
+    DefaultJsproperty = api::napi_property_attributes_napi_default_jsproperty,
+}
+
+/// napi_valuetype
+#[repr(u32)]
+pub enum NapiValuetype {
+    Undefined = api::napi_valuetype_napi_undefined,
+    Null = api::napi_valuetype_napi_null,
+    Boolean = api::napi_valuetype_napi_boolean,
+    Number = api::napi_valuetype_napi_number,
+    String = api::napi_valuetype_napi_string,
+    Symbol = api::napi_valuetype_napi_symbol,
+    Object = api::napi_valuetype_napi_object,
+    Function = api::napi_valuetype_napi_function,
+    External = api::napi_valuetype_napi_external,
+    Bigint = api::napi_valuetype_napi_bigint,
+}
+
+/// napi_typedarray_type
+#[repr(u32)]
+pub enum NapiTypedarrayType {
+    Int8Array = api::napi_typedarray_type_napi_int8_array,
+    Uint8Array = api::napi_typedarray_type_napi_uint8_array,
+    Uint8ClampedArray = api::napi_typedarray_type_napi_uint8_clamped_array,
+    Int16Array = api::napi_typedarray_type_napi_int16_array,
+    Uint16Array = api::napi_typedarray_type_napi_uint16_array,
+    Int32Array = api::napi_typedarray_type_napi_int32_array,
+    Uint32Array = api::napi_typedarray_type_napi_uint32_array,
+    Float32Array = api::napi_typedarray_type_napi_float32_array,
+    Float64Array = api::napi_typedarray_type_napi_float64_array,
+    Bigint64Array = api::napi_typedarray_type_napi_bigint64_array,
+    Biguint64Array = api::napi_typedarray_type_napi_biguint64_array,
+}
+
+/// napi_status
 #[repr(u32)]
 pub enum NapiStatus {
     Ok = api::napi_status_napi_ok,
