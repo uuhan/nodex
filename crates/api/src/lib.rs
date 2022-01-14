f846db50ef1d10d013557735d8b5cfb52840b6ae
uuhan <xuminhui189@gmail.com>
2022-01-14 18:09:03 +0800

    node_api_rs::NapiStatus

--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -5,32 +5,32 @@
 
 pub mod api {
     include!(concat!(env!("OUT_DIR"), "/api.rs"));
+}
 
-    #[repr(u32)]
-    pub enum NapiStatus {
-        Ok = napi_status_napi_ok,
-        InvalidArg = napi_status_napi_invalid_arg,
-        ObjectExpected = napi_status_napi_object_expected,
-        StringExpected = napi_status_napi_string_expected,
-        NameExpected = napi_status_napi_name_expected,
-        FunctionExpected = napi_status_napi_function_expected,
-        NumberExpected = napi_status_napi_number_expected,
-        BooleanExpected = napi_status_napi_boolean_expected,
-        ArrayExpected = napi_status_napi_array_expected,
-        GenericFailure = napi_status_napi_generic_failure,
-        PendingException = napi_status_napi_pending_exception,
-        Cancelled = napi_status_napi_cancelled,
-        EscapeCalledTwice = napi_status_napi_escape_called_twice,
-        HandleScopeMismatch = napi_status_napi_handle_scope_mismatch,
-        CallbackScopeMismatch = napi_status_napi_callback_scope_mismatch,
-        QueueFull = napi_status_napi_queue_full,
-        Closing = napi_status_napi_closing,
-        BigintExpected = napi_status_napi_bigint_expected,
-        DateExpected = napi_status_napi_date_expected,
-        ArraybufferExpected = napi_status_napi_arraybuffer_expected,
-        DetachableArraybufferExpected = napi_status_napi_detachable_arraybuffer_expected,
-        WouldDeadlock = napi_status_napi_would_deadlock,
-    }
+#[repr(u32)]
+pub enum NapiStatus {
+    Ok = api::napi_status_napi_ok,
+    InvalidArg = api::napi_status_napi_invalid_arg,
+    ObjectExpected = api::napi_status_napi_object_expected,
+    StringExpected = api::napi_status_napi_string_expected,
+    NameExpected = api::napi_status_napi_name_expected,
+    FunctionExpected = api::napi_status_napi_function_expected,
+    NumberExpected = api::napi_status_napi_number_expected,
+    BooleanExpected = api::napi_status_napi_boolean_expected,
+    ArrayExpected = api::napi_status_napi_array_expected,
+    GenericFailure = api::napi_status_napi_generic_failure,
+    PendingException = api::napi_status_napi_pending_exception,
+    Cancelled = api::napi_status_napi_cancelled,
+    EscapeCalledTwice = api::napi_status_napi_escape_called_twice,
+    HandleScopeMismatch = api::napi_status_napi_handle_scope_mismatch,
+    CallbackScopeMismatch = api::napi_status_napi_callback_scope_mismatch,
+    QueueFull = api::napi_status_napi_queue_full,
+    Closing = api::napi_status_napi_closing,
+    BigintExpected = api::napi_status_napi_bigint_expected,
+    DateExpected = api::napi_status_napi_date_expected,
+    ArraybufferExpected = api::napi_status_napi_arraybuffer_expected,
+    DetachableArraybufferExpected = api::napi_status_napi_detachable_arraybuffer_expected,
+    WouldDeadlock = api::napi_status_napi_would_deadlock,
 }
 
 pub mod env;
