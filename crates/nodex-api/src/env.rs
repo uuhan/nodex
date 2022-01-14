d175ff536bc690dbed942c2d211848e604ea4ccb
uuhan <xuminhui189@gmail.com>
2022-01-15 01:10:17 +0800

    impl Env struct

--- crates/nodex-api/src/env.rs
+++ crates/nodex-api/src/env.rs
@@ -1,10 +1,26 @@
+use std::{
+    marker::PhantomData,
+};
+
 use crate::api::napi_env;
 
-#[derive(Clone, Debug)]
-pub struct Env(napi_env);
+#[derive(Clone, Copy, Debug)]
+pub struct Env<'a>(napi_env, PhantomData<&'a napi_env>);
 
-impl AsRef<napi_env> for Env {
+impl<'a> AsRef<napi_env> for Env<'a> {
     fn as_ref(&self) -> &napi_env {
         &self.0
+    }
+}
+
+impl<'a> Env<'a> {
+    /// create `Env` from raw napi_env
+    pub fn from_raw(env: napi_env) -> Env<'a> {
+        Env(env, PhantomData)
+    }
+
+    /// access raw napi_env from `Env`
+    pub fn raw(&self) -> napi_env {
+        self.0
     }
 }
