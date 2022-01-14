2b0991838d112f432942203d61e718cd8a1feb9e
uuhan <xuminhui189@gmail.com>
2022-01-14 17:02:59 +0800

    clippy

--- examples/demo/src/lib.rs
+++ examples/demo/src/lib.rs
@@ -2,6 +2,6 @@ use node_api_rs::api;
 
 node_api_rs::init!(init);
 
-fn init(env: api::napi_env, exports: api::napi_value) {
+fn init(_env: api::napi_env, _exports: api::napi_value) {
     todo!()
 }
