455b320d1a74a2157e6ef045f3eed31b125b2ed3
uuhan <xuminhui189@gmail.com>
2022-01-18 16:31:51 +0800

    feat(nodex-api): impl NapiAsyncWork, queue, cancel, delete

--- crates/nodex-api/src/work.rs
+++ crates/nodex-api/src/work.rs
@@ -21,25 +21,52 @@ impl NapiAsyncWork {
     /// async_resource_name should be a null-terminated, UTF-8-encoded string.
     /// The async_resource_name identifier is provided by the user and should be representative of the type of async work being performed. It is also recommended to apply namespacing to the identifier, e.g. by including the module name. See the async_hooks documentation for more information.
     pub fn new(env: NapiEnv, name: impl AsRef<str>) -> NapiResult<NapiAsyncWork> {
-        let work = unsafe {
-            let mut result = MaybeUninit::uninit();
-            let status = api::napi_create_async_work(
-                env.raw(),
-                env.object()?.raw(),
-                env.string(name)?.raw(),
-                None,
-                None,
-                std::ptr::null_mut(),
-                result.as_mut_ptr(),
-            );
-
-            if status.err() {
-                return Err(status);
-            }
-
-            result.assume_init()
-        };
+        let work = napi_call!(
+            =napi_create_async_work,
+            env.raw(),
+            env.object()?.raw(),
+            env.string(name)?.raw(),
+            None,
+            None,
+            std::ptr::null_mut(),
+        );
 
         Ok(NapiAsyncWork(env, work))
+    }
+
+    /// This API requests that the previously allocated work be scheduled for execution. Once it
+    /// returns successfully, this API must not be called again with the same napi_async_work item
+    /// or the result will be undefined.
+    pub fn queue(&self) -> NapiResult<()> {
+        Ok(napi_call!(
+            napi_queue_async_work,
+            self.env().raw(),
+            self.raw(),
+        ))
+    }
+
+    /// This API cancels queued work if it has not yet been started. If it has already
+    /// started executing, it cannot be cancelled and napi_generic_failure will be returned.
+    /// If successful, the complete callback will be invoked with a status value of
+    /// napi_cancelled. The work should not be deleted before the complete callback invocation,
+    /// even if it has been successfully cancelled.
+    ///
+    /// This API can be called even if there is a pending JavaScript exception.
+    pub fn cancel(&self) -> NapiResult<()> {
+        Ok(napi_call!(
+            napi_cancel_async_work,
+            self.env().raw(),
+            self.raw(),
+        ))
+    }
+
+    /// This API frees a previously allocated work object.
+    /// This API can be called even if there is a pending JavaScript exception.
+    pub fn delete(&self) -> NapiResult<()> {
+        Ok(napi_call!(
+            napi_delete_async_work,
+            self.env().raw(),
+            self.raw(),
+        ))
     }
 }
