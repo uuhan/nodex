ebe2b17f1734bc60f130155b038e19b57a2f5fd3
uuhan <xuminhui189@gmail.com>
2022-01-29 21:37:53 +0800

    feat(examples): descriptor example

--- examples/descriptor.js
+++ examples/descriptor.js
@@ -3,5 +3,5 @@ const descriptor = require("./descriptor.node")
 console.log(descriptor.obj.myvalue)
 console.log(descriptor.obj.mymethod())
 
-descriptor.obj.myaccessor = 200.
+descriptor.obj.myaccessor = 400.
 console.log(descriptor.obj.myaccessor)
