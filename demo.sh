ccc039eeed1cb11da79c140f2a857fabb36c10a2
uuhan <xuminhui189@gmail.com>
2022-02-03 11:46:05 +0800

    demo.sh

---
+++ demo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+bash examples/demo.sh
+bash examples/descriptor.sh
+bash examples/element.sh
+bash examples/func.sh
+bash examples/plugin.sh
+bash examples/promise.sh
+bash examples/script.sh
+bash examples/tsfn.sh
