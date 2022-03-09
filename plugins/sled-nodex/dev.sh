9e7f5f2a6b37fa1750deab100dc4a05b5ec3ea2b
uuhan <xuminhui189@gmail.com>
2022-03-09 16:42:05 +0800

    feat(plugin): sled dev.sh

--- plugins/sled-nodex/dev.sh
+++ plugins/sled-nodex/dev.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+pushd $(dirname $0) 2>/dev/null
+
 if [[ -n "${1}" ]]; then
   echo ${1} ' build'
   cargo build --${1:-release}
@@ -8,6 +10,10 @@ else
 fi
 
 rm -rf "sled.node" 2>/dev/null
-cp ../../target/${1:-debug}/libsled_nodex.dylib sled.node
+
+[[ -f ${FILE:="../../target/${1:-debug}/libsled_nodex.dylib"} ]] && cp ${FILE} sled.node
+[[ -f ${FILE:="../../target/${1:-debug}/libsled_nodex.so"} ]] && cp ${FILE} sled.node
 
 node --napi-modules -e 'const sled = require("./sled.node"); console.log(sled)'
+
+popd 2>/dev/null
